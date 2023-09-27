cdffd125e9fd9b2e48e1e0cd1565c93706627113
Guru Sanjay R K <gurucoolmsd@gmail.com>
2023-09-27 21:13:13 +0530

    Added custom fonts

--- SpeechToText/tailwind.config.js
+++ SpeechToText/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kanit':["Kanit"],
+        'oswald':["Oswald"]
+        
+      }
+    },
   },
   plugins: [],
 }
